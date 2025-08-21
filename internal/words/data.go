5f7e382ddc486b115119b6d45b9eea352eed6e60
Марат <marat.tskh@gmail.com>
2025-08-21 10:44:42 +0300

    third task fixes

--- internal/words/data.go
+++ internal/words/data.go
@@ -25,6 +25,39 @@ func (r *Repo) RGetWordById(id int) (*Word, error) {
 	return &word, nil
 }
 
+// Ищет 100 похожих слов
+func (r *Repo) RSearchClosestWords(title string) ([]Word, error) {
+	// фильтруем базу
+	rows, err := r.db.Query(`
+        SELECT id, title, translation,
+        FROM ru_en 
+        WHERE similarity(title, $1) >= 0.2
+        ORDER BY sim DESC
+        LIMIT 100`,
+		title,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Собираем 100 похожих слов
+	var closestsWords []Word
+	for rows.Next() {
+		var closestWord Word
+		err := rows.Scan(&closestWord.Id, &closestWord.Title, &closestWord.Translation)
+		if err != nil {
+			return nil, err
+		}
+		closestsWords = append(closestsWords, closestWord)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return closestsWords, nil
+}
+
 // CreateNewWords добавляет новые переводы в базу даных
 func (r *Repo) CreateNewWords(word, translate string) error {
 	_, err := r.db.Exec(`INSERT INTO ru_en (title, translation) VALUES ($1, $2)`, word, translate)
